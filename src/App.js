ac3d82895584e24636ca52e83589ff5d0f62b703
AdraProductStudio <adraforyou@gmail.com>
2024-07-06 18:20:34 +0530

    FEATURE : model rocket web-app

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Component/Home";
+import AuthPage from "./Component/AuthPage";
+import ConsumerPreferenceLayout from "../src/Component/Reusables/ConsumerPreferenceLayout";
+import { Toaster } from "react-hot-toast";
+import ProductCategory from "./Component/Reusables/ProductCategory";
+import AdminForm from "./Component/AdminForm";
+import { DataProvider } from "./Component/CommonContext";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <DataProvider>
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<AuthPage />}>
+            <Route index exact element={<Home />} />
+            <Route path="admin" element={<AdminForm />} />
+
+            <Route path="consumer_preference" exact element={<ConsumerPreferenceLayout />} />
+
+
+            <Route path="category" element={<ProductCategory />} />
+
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+          </Route>
+        </Routes>
+      </DataProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
