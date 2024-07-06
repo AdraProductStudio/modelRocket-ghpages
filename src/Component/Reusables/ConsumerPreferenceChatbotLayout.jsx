3fb093c358277020442a0be8b420a6b9ba56506e
AdraProductStudio <adraforyou@gmail.com>
2024-07-06 20:54:32 +0530

    UPDATE : reset button working

--- src/Component/Reusables/ConsumerPreferenceChatbotLayout.jsx
+++ src/Component/Reusables/ConsumerPreferenceChatbotLayout.jsx
@@ -416,6 +416,8 @@ const ConsumerPreferenceChatbotLayout = () => {
         setMessages([]);
         setShowGraphSection(false)
         setDotIconDropdown(false);
+        setProductComparison([])
+        setViewGraph(false)
     };
 
 
